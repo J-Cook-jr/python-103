3a734f69aa7c57be1ee1ebf2e6dc5b018e4eba25
Jeff Cook <jecook2692@gmail.com>
2020-04-02 14:16:03 -0400

    adds count prompt

---
+++ exercise8.py
@@ -0,0 +1,7 @@
+#This is a program that prompts the user for a start and an end number.
+#starting from the start number the program will count until it reaches the end number.
+
+#Prompt the user for a number to start counting from.
+count = int(input('Start from number: '))
+
+
