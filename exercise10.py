ccd3975f8cb2421322a4fdfc0388f93a65f78b9f
Jeff Cook <jecook2692@gmail.com>
2020-04-02 23:26:39 -0400

    adds first block

---
+++ exercise10.py
@@ -0,0 +1,8 @@
+# This program prints a star pattern square the size of the users choice.
+
+# The following block of code prompts the user to enter a number and defines the first variable as a default variable.
+row = int(input('Enter any number to define the size of your square: '))
+r = 0
+
+# The following line of code tells the program to loop through variable r as long as it's less than row.
+while r < row:
