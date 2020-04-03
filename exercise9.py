fe39f15aa764e3d026906517aea855004ea2793d
Jeff Cook <jecook2692@gmail.com>
2020-04-02 22:35:10 -0400

    adds row

--- exercise9.py
+++ exercise9.py
@@ -1,1 +1,11 @@
 #Print a 5 x 5 block of '*' characters.
+
+#Define two variables: One variable tells the computer how many rows and the other will be a default variable.
+row = int(5)
+r = 0
+
+#This line tells the computer to loop through the r variable as long as it's less than row.
+while r < row:
+
+
+
